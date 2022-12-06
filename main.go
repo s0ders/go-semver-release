37e3a1946c37e7cc44a99cd972d18e65dd151e95
s0ders <39492740+s0ders@users.noreply.github.com>
2022-12-06 18:27:58 +0100

    feat: now handles custom release rules and Git access token for auth.

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 // go-semver-release package aims to be a simple
 // program for CI/CD runner that applies the semver
 // spec. and conventional commit spec. to a Git repository
-// so that version // number are automatically and reliably
+// so that version number are automatically and reliably
 // handled by Git annotated tags.
 package main
 
@@ -15,39 +15,46 @@ import (
 
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/storage/memory"
+	"github.com/go-git/go-git/v5/plumbing/transport/http"
 )
 
-// TODO: Read GitHub/GitLab access token from environment or flag
 // TODO: Properly handle errors
-// TODO: Add a --rules flag that takes a JSON file defining release rule
 // TODO: Add a --verbose flag to enable verbose output
 func main() {
 	gitUrl := flag.String("url", "", "The Git repository to work on")
-	releaseRules := flag.String("rules", "", "Path to a JSON file containing the rules for releasing new version based on commit types.")
+	releaseRulesPath := flag.String("rules", "", "Path to a JSON file containing the rules for releasing new version based on commit types")
+	accessTokenFlag := flag.String("access-token", "", "A personnal access token to push tag to the Git repository")
 	flag.Parse()
 
 	if *gitUrl == "" {
 		log.Fatalf("--url cannot be empty\n")
 	}
 
+	if *accessTokenFlag == "" {
+		log.Fatalf("--access-token cannot be nul")
+	}
+
 	r, err := git.Clone(memory.NewStorage(), nil, &git.CloneOptions{
+		Auth: &http.BasicAuth{
+			Username: "go-semver-release",
+			Password: *accessTokenFlag,
+		},
 		URL: *gitUrl,
+		Progress: os.Stdout,
 	})
+
 	failOnError(err)
 
-	// Fetch all tags from Git repository
 	tags, err := r.TagObjects()
 	failOnError(err)
 
-	commitAnalyzer := commitanalyzer.NewCommitAnalyzer(log.New(os.Stdout, "[commit-analyzer] ", log.Lshortfile))
-
-	// Fetch latest semver tag HERE
+	commitAnalyzer := commitanalyzer.NewCommitAnalyzer(log.New(os.Stdout, "[commit-analyzer] ", log.Default().Flags()), releaseRulesPath)
 	latestSemverTag := commitAnalyzer.FetchLatestSemverTag(tags)
 
 	commitHistory, err := r.Log(&git.LogOptions{Since: &latestSemverTag.Tagger.When})
 	failOnError(err)
 	
-	semver := commitAnalyzer.ComputeNewSemverNumber(commitHistory, latestSemverTag, releaseRules)
+	semver := commitAnalyzer.ComputeNewSemverNumber(commitHistory, latestSemverTag)
 
 	fmt.Println("Semver: ", semver)
 }
