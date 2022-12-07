acef51a9038c82debf57a31a18b3885572d47bab
s0ders <39492740+s0ders@users.noreply.github.com>
2022-12-07 01:41:58 +0100

    feat: added main logger and push tag to remote if any

--- main.go
+++ main.go
@@ -7,31 +7,30 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
 	"os"
 
 	"github.com/s0ders/go-semver-release/commitanalyzer"
+	"github.com/s0ders/go-semver-release/tagger"
 
+	"github.com/go-git/go-git/v5/config"
 	"github.com/go-git/go-git/v5"
-	"github.com/go-git/go-git/v5/storage/memory"
 	"github.com/go-git/go-git/v5/plumbing/transport/http"
+	"github.com/go-git/go-git/v5/storage/memory"
 )
 
 // TODO: Properly handle errors
 // TODO: Add a --verbose flag to enable verbose output
+// TODO: Add a --dry-run flag
 func main() {
+	logger := log.New(os.Stdout, "[main] ", log.Default().Flags())
 	gitUrl := flag.String("url", "", "The Git repository to work on")
 	releaseRulesPath := flag.String("rules", "", "Path to a JSON file containing the rules for releasing new version based on commit types")
-	accessTokenFlag := flag.String("access-token", "", "A personnal access token to push tag to the Git repository")
+	accessTokenFlag := flag.String("token", "", "A personnal access token to push tag to the Git repository")
 	flag.Parse()
 
 	if *gitUrl == "" {
-		log.Fatalf("--url cannot be empty\n")
-	}
-
-	if *accessTokenFlag == "" {
-		log.Fatalf("--access-token cannot be nul")
+		logger.Fatal("--url cannot be empty\n")
 	}
 
 	r, err := git.Clone(memory.NewStorage(), nil, &git.CloneOptions{
@@ -39,28 +38,56 @@ func main() {
 			Username: "go-semver-release",
 			Password: *accessTokenFlag,
 		},
-		URL: *gitUrl,
+		URL:      *gitUrl,
 		Progress: os.Stdout,
 	})
 
-	failOnError(err)
+	if err != nil {
+		logger.Fatalf("Failed to clone repository: %s", err)
+	}
 
 	tags, err := r.TagObjects()
-	failOnError(err)
+	
+	if err != nil {
+		logger.Fatalf("Failed to fetch tags: %s", err)
+	}
 
 	commitAnalyzer := commitanalyzer.NewCommitAnalyzer(log.New(os.Stdout, "[commit-analyzer] ", log.Default().Flags()), releaseRulesPath)
+
+	// Fetch all semantic versioning tags (i.e. vX.Y.Z) from the repository
 	latestSemverTag := commitAnalyzer.FetchLatestSemverTag(tags)
 
 	commitHistory, err := r.Log(&git.LogOptions{Since: &latestSemverTag.Tagger.When})
-	failOnError(err)
-	
-	semver := commitAnalyzer.ComputeNewSemverNumber(commitHistory, latestSemverTag)
-
-	fmt.Println("Semver: ", semver)
-}
-
-func failOnError(e error) {
-	if e != nil {
-		log.Fatalf("Error: %s\n", e)
+	if err != nil {
+		logger.Fatalf("Failed to fetch commit history: %s", err)
 	}
+
+	// Compute the next semantic versioning number
+	semver, noNewVersion := commitAnalyzer.ComputeNewSemverNumber(commitHistory, latestSemverTag)
+	
+	if noNewVersion {
+		logger.Printf("No new version, still on %s", semver)
+		os.Exit(0)
+	}
+
+	t := tagger.NewTagger(log.New(os.Stdout, "[tagger] ", log.Default().Flags()))
+	r = t.CreateAndPushNewTag(r, semver)
+
+	po := &git.PushOptions{
+		RemoteName: "origin",
+		Progress:   os.Stdout,
+		RefSpecs:   []config.RefSpec{config.RefSpec("refs/tags/*:refs/tags/*")},
+		Auth:       &http.BasicAuth{
+			Username: "go-semver-release",
+			Password: *accessTokenFlag,
+		},
+	}
+	
+	// Push tag to remote
+	err = r.Push(po)
+	if err != nil {
+		logger.Fatalf("Failed to push tag: %s", err)
+	}
+	logger.Printf("Pushed tag %s on repository", semver)
+	os.Exit(0)
 }
