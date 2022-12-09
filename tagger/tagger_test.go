830a207241bb9ede4035dc0f7d41026c109a762f
s0ders <39492740+s0ders@users.noreply.github.com>
2022-12-09 18:16:19 +0100

    test: added createGitRepository func

--- tagger/tagger_test.go
+++ tagger/tagger_test.go
@@ -1,6 +1,7 @@
 package tagger
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"path/filepath"
@@ -9,56 +10,17 @@ import (
 
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing/object"
+	"github.com/s0ders/go-semver-release/semver"
 )
 
 func TestTagExists(t *testing.T) {
 
-	tempDirPath, err := os.MkdirTemp("", "tagger-*")
+	r, repositoryPath, err := createGitRepository("fix: commit that trigger a patch release")
 	if err != nil {
-		t.Fatalf("failed to create temp directory: %s", err)
+		t.Fatalf("failed to create git repository: %s", err)
 	}
 
-	defer os.RemoveAll(tempDirPath)
-
-	r, err := git.PlainInit(tempDirPath, false)
-	if err != nil {
-		t.Fatalf("failed to initialize git repository: %s", err)
-	}
-
-	w, err := r.Worktree()
-	if err != nil {
-		t.Fatalf("failed to get worktree: %s", err)
-	}
-
-	tempFileName := "temp"
-	tempFilePath := filepath.Join(tempDirPath, tempFileName)
-	_, err = os.Create(tempFilePath)
-	if err != nil {
-		t.Fatalf("failed to create temp file: %s", err)
-	}
-
-	err = os.WriteFile(tempFilePath, []byte("..."), 0644)
-	if err != nil {
-		t.Fatalf("failed to write to temp file: %s", err)
-	}
-
-	_, err = w.Add(tempFileName)
-	if err != nil {
-		t.Fatalf("failed to add temp file to worktree: %s", err)
-	}
-
-	commit, err := w.Commit("firt commit", &git.CommitOptions{
-		Author: &object.Signature{
-			Name:  "Go Semver Release",
-			Email: "go-semver-release@ci.go",
-			When:  time.Now(),
-		},
-	})
-
-	_, err = r.CommitObject(commit)
-	if err != nil {
-		t.Fatalf("failed to commit object %s", err)
-	}
+	defer os.RemoveAll(repositoryPath)
 
 	h, err := r.Head()
 	if err != nil {
@@ -84,7 +46,6 @@ func TestTagExists(t *testing.T) {
 	if err != nil {
 		t.Fatalf("failed to check if tag exists: %s", err)
 	}
-
 	if want := true; tagExistsTrue != want {
 		t.Fatalf("got: %t want: %t", tagExistsTrue, want)
 	}
@@ -93,8 +54,87 @@ func TestTagExists(t *testing.T) {
 	if err != nil {
 		t.Fatalf("failed to check if tag exists: %s", err)
 	}
-
 	if want := false; tagExistsFalse != want {
 		t.Fatalf("got: %t want: %t", tagExistsFalse, want)
 	}
+}
+
+func TestAddTagToRepository(t *testing.T) {
+	r, repositoryPath, err := createGitRepository("fix: commit that trigger a patch release")
+	if err != nil {
+		t.Fatalf("failed to create git repository: %s", err)
+	}
+
+	defer os.RemoveAll(repositoryPath)
+
+	semver, err := semver.NewSemver(1, 0, 0, "")
+	if err != nil {
+		t.Fatalf("failed to create semver: %s", err)
+	}
+
+	tagger := NewTagger(log.Default(), "")
+	taggedRepository, err := tagger.AddTagToRepository(r, semver)
+	if err != nil {
+		t.Fatalf("failed to tag repository: %s", err)
+	}
+
+	tagExists, err := tagger.TagExists(taggedRepository, semver.String())
+	if err != nil {
+		t.Fatalf("failed to check if tag exists: %s", err)
+	}
+
+	if want := true; tagExists != want {
+		t.Fatalf("want: %t got: %t", want, tagExists)
+	}
+}
+
+
+func createGitRepository(firstCommitMessage string) (*git.Repository, string, error) {
+
+	tempDirPath, err := os.MkdirTemp("", "tagger-*")
+	if err != nil {
+		return nil, "", fmt.Errorf("failed to create temp directory: %w", err)
+	}
+
+	r, err := git.PlainInit(tempDirPath, false)
+	if err != nil {
+		return nil, "", fmt.Errorf("failed to initialize git repository: %s", err)
+	}
+
+	w, err := r.Worktree()
+	if err != nil {
+		return nil, "", fmt.Errorf("failed to get worktree: %s", err)
+	}
+
+	tempFileName := "temp"
+	tempFilePath := filepath.Join(tempDirPath, tempFileName)
+	_, err = os.Create(tempFilePath)
+	if err != nil {
+		return nil, "", fmt.Errorf("failed to create temp file: %s", err)
+	}
+
+	err = os.WriteFile(tempFilePath, []byte("Hello world"), 0644)
+	if err != nil {
+		return nil, "", fmt.Errorf("failed to write to temp file: %s", err)
+	}
+
+	_, err = w.Add(tempFileName)
+	if err != nil {
+		return nil, "", fmt.Errorf("failed to add temp file to worktree: %s", err)
+	}
+
+	commit, err := w.Commit(firstCommitMessage, &git.CommitOptions{
+		Author: &object.Signature{
+			Name:  "Go Semver Release",
+			Email: "go-semver-release@ci.go",
+			When:  time.Now(),
+		},
+	})
+
+	_, err = r.CommitObject(commit)
+	if err != nil {
+		return nil, "", fmt.Errorf("failed to commit object %s", err)
+	}
+
+	return r, tempDirPath, nil
 }
