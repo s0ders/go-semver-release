378535c765133c9bcca87d7a1e87db2edc6e60d2
s0ders <39492740+s0ders@users.noreply.github.com>
2024-04-29 21:54:51 +0200

    test: added test for versionCmd

---
+++ cmd/version_test.go
@@ -0,0 +1,20 @@
+package cmd
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCmd_Version(t *testing.T) {
+	assert := assert.New(t)
+
+	actual := new(bytes.Buffer)
+	rootCmd.SetOut(actual)
+	rootCmd.SetErr(actual)
+	rootCmd.SetArgs([]string{"version"})
+
+	err := rootCmd.Execute()
+	assert.NoError(err, "local command executed with error")
+}
