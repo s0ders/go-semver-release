6999d3e1643dc03363fb04bbb3f457851ac90e54
s0ders <39492740+s0ders@users.noreply.github.com>
2024-05-01 22:35:24 +0200

    test: test case with unknown command

--- cmd/root_test.go
+++ cmd/root_test.go
@@ -17,3 +17,15 @@ func TestRootCmd_NoError(t *testing.T) {
 	err := rootCmd.Execute()
 	assert.NoError(err, "should not have failed running rootCmd")
 }
+
+func TestRootCmd_Error(t *testing.T) {
+	assert := assert.New(t)
+
+	actual := new(bytes.Buffer)
+	rootCmd.SetOut(actual)
+	rootCmd.SetErr(actual)
+	rootCmd.SetArgs([]string{"unknown"})
+
+	err := rootCmd.Execute()
+	assert.Error(err, "should not have failed trying to run unknown command")
+}
