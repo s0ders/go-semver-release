8010e2d675d5bed326f1d8bc7237f6d26aeaed8c
s0ders <39492740+s0ders@users.noreply.github.com>
2024-05-01 15:08:50 +0200

    refactor: made verbose a persistent flag in rootCmd

--- cmd/root.go
+++ cmd/root.go
@@ -4,6 +4,12 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var verbose bool
+
+func init() {
+	rootCmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "Verbose ci")
+}
+
 var rootCmd = &cobra.Command{
 	Use:   "go-semver-release",
 	Short: "go-semver-release - CLI to automate semantic versioning of git repositories",
