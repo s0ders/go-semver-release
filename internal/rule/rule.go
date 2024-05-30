a876a38297d6ab20857d48b0ed99d7be685e4e87
s0ders <39492740+s0ders@users.noreply.github.com>
2024-05-30 22:49:42 +0200

    refactor(internal/rule): changed how rules are handled (viper)

--- internal/rule/rule.go
+++ internal/rule/rule.go
@@ -1,13 +1,10 @@
-// Package rule provides functions to deal with release rule.
 package rule
 
-import (
-	"bytes"
-	"encoding/json"
-	"errors"
-	"fmt"
-	"io"
-)
+import "errors"
+
+type Rules struct {
+	Unmarshalled map[string][]string
+}
 
 var (
 	ErrInvalidCommitType    = errors.New("invalid commit type")
@@ -35,96 +32,34 @@ var validReleaseTypes = map[string]struct{}{
 	"patch": {},
 }
 
-type ReleaseRule struct {
-	CommitType  string `json:"type"`
-	ReleaseType string `json:"release"`
-}
-
-type ReleaseRules struct {
-	Rules []ReleaseRule `json:"rule"`
-}
-
-var Default = ReleaseRules{Rules: []ReleaseRule{
-	{"feat", "minor"},
-	{"fix", "patch"},
-	{"perf", "patch"},
-	{"revert", "patch"},
-}}
-
-type Options struct {
-	Reader io.Reader
-}
-
-type OptionFunc func(*Options)
-
-func WithReader(reader io.Reader) OptionFunc {
-	return func(o *Options) {
-		o.Reader = reader
+func (r Rules) Validate() error {
+	if len(r.Unmarshalled) == 0 {
+		return ErrNoRules
 	}
+
+	for releaseType, commitTypes := range r.Unmarshalled {
+		if _, ok := validReleaseTypes[releaseType]; !ok {
+			return ErrInvalidReleaseType
+		}
+
+		for _, commitType := range commitTypes {
+			if _, ok := validCommitTypes[commitType]; !ok {
+				return ErrInvalidCommitType
+			}
+		}
+	}
+
+	return nil
 }
 
-// Map returns a flat map corresponding to the release rule with commit types as keys and release types as values.
-func (r *ReleaseRules) Map() map[string]string {
-	m := make(map[string]string, len(r.Rules))
-	for _, rule := range r.Rules {
-		m[rule.CommitType] = rule.ReleaseType
+func (r Rules) Map() map[string]string {
+	m := make(map[string]string)
+
+	for releaseType, commitTypes := range r.Unmarshalled {
+		for _, commitType := range commitTypes {
+			m[commitType] = releaseType
+		}
 	}
+
 	return m
-}
-
-// Init initialize a new set of release rule with the given options if any.
-func Init(options ...OptionFunc) (ReleaseRules, error) {
-
-	opts := &Options{}
-
-	for _, optionFunc := range options {
-		optionFunc(opts)
-	}
-
-	if opts.Reader == nil {
-		return Default, nil
-	}
-
-	return Parse(opts.Reader)
-}
-
-// Parse reads a buffer a returns the corresponding release rule.
-func Parse(reader io.Reader) (ReleaseRules, error) {
-	var rules ReleaseRules
-	existingType := make(map[string]string)
-
-	buf, err := io.ReadAll(reader)
-	if err != nil {
-		return ReleaseRules{}, fmt.Errorf("reading rule file: %w", err)
-	}
-
-	bufReader := bytes.NewReader(buf)
-
-	decoder := json.NewDecoder(bufReader)
-	err = decoder.Decode(&rules)
-	if err != nil {
-		return ReleaseRules{}, fmt.Errorf("decoding JSON into rule: %w", err)
-	}
-
-	for _, rule := range rules.Rules {
-		if _, ok := validCommitTypes[rule.CommitType]; !ok {
-			return ReleaseRules{}, ErrInvalidCommitType
-		}
-
-		if _, ok := validReleaseTypes[rule.ReleaseType]; !ok {
-			return ReleaseRules{}, ErrInvalidReleaseType
-		}
-
-		if _, ok := existingType[rule.CommitType]; ok {
-			return ReleaseRules{}, ErrDuplicateReleaseRule
-		}
-
-		existingType[rule.CommitType] = rule.ReleaseType
-	}
-
-	if len(rules.Rules) == 0 {
-		return ReleaseRules{}, ErrNoRules
-	}
-
-	return rules, nil
 }
--- internal/rule/rule_test.go
+++ internal/rule/rule_test.go
@@ -1,141 +1,42 @@
 package rule
 
 import (
-	"os"
-	"path/filepath"
-	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestRules_Map(t *testing.T) {
+func TestRule_Validate(t *testing.T) {
 	assert := assert.New(t)
-
-	rules, err := Init()
-	assert.NoError(err, "should have been able to parse rule")
-
-	got := rules.Map()
-	want := map[string]string{
-		"feat":   "minor",
-		"fix":    "patch",
-		"perf":   "patch",
-		"revert": "patch",
-	}
-
-	assert.Equal(want, got, "rule maps should match")
-}
-
-func TestRules_ParseDefault(t *testing.T) {
-	assert := assert.New(t)
-
-	rules, err := Init()
-	assert.NoError(err, "should have been able to parse rule")
 
 	type test struct {
-		commitType  string
-		releaseType string
+		have Rules
+		want error
 	}
 
-	matrix := []test{
-		{"feat", "minor"},
-		{"fix", "patch"},
-		{"perf", "patch"},
-		{"revert", "patch"},
+	tests := []test{
+		{have: Rules{Unmarshalled: map[string][]string{"minor": {"feat"}, "patch": {"fix", "perf"}}}, want: nil},
+		{have: Rules{Unmarshalled: map[string][]string{"unknown": {"feat"}, "patch": {"perf"}}}, want: ErrInvalidReleaseType},
+		{have: Rules{Unmarshalled: map[string][]string{"minor": {"unknown"}, "patch": {"perf"}}}, want: ErrInvalidCommitType},
 	}
 
-	for i := 0; i < len(rules.Rules); i++ {
-		got := rules.Rules[i]
-		want := matrix[i]
-
-		assert.Equal(want.commitType, got.CommitType, "commit type should match")
-		assert.Equal(want.releaseType, got.ReleaseType, "release type should match")
+	for _, tc := range tests {
+		assert.Equal(tc.want, tc.have.Validate())
 	}
 }
 
-func TestRules_DuplicateType(t *testing.T) {
+func TestRule_Map(t *testing.T) {
 	assert := assert.New(t)
 
-	const duplicateRules = `{
-		"rule": [
-			{"type": "feat", "release": "minor"},
-			{"type": "feat", "release": "patch"}
-		]
-	}`
+	have := Rules{
+		Unmarshalled: map[string][]string{"minor": {"feat"}, "patch": {"fix", "perf"}},
+	}
 
-	reader := strings.NewReader(duplicateRules)
+	want := map[string]string{
+		"feat": "minor",
+		"fix":  "patch",
+		"perf": "patch",
+	}
 
-	_, err := Init(WithReader(reader))
-	assert.ErrorIs(err, ErrDuplicateReleaseRule, "should have detected incorrect rule")
-}
-
-func TestRules_NoRules(t *testing.T) {
-	assert := assert.New(t)
-
-	reader := strings.NewReader(`{"rule": []}`)
-
-	_, err := Init(WithReader(reader))
-	assert.ErrorIs(err, ErrNoRules, "should have detected empty rule")
-}
-
-func TestRules_InvalidType(t *testing.T) {
-	assert := assert.New(t)
-
-	const duplicateRules = `{
-		"rule": [
-			{"type": "feat", "release": "minor"},
-			{"type": "unknown", "release": "patch"}
-		]
-	}`
-
-	reader := strings.NewReader(duplicateRules)
-
-	_, err := Init(WithReader(reader))
-	assert.ErrorIs(err, ErrInvalidCommitType, "should have detected incorrect rule")
-}
-
-func TestRules_InvalidRelease(t *testing.T) {
-	assert := assert.New(t)
-
-	// Using a "release" of type major is forbidden since they are
-	// reserved for breaking changes.
-	const duplicateRules = `{
-		"rule": [
-			{"type": "feat", "release": "minor"},
-			{"type": "fix", "release": "major"}
-		]
-	}`
-
-	reader := strings.NewReader(duplicateRules)
-
-	_, err := Init(WithReader(reader))
-	assert.ErrorIs(err, ErrInvalidReleaseType, "should have detected incorrect rule")
-}
-
-func TestRules_EmptyFile(t *testing.T) {
-	assert := assert.New(t)
-
-	tempDir, err := os.MkdirTemp("", "rule-*")
-	assert.NoError(err, "failed to create temp. dir.")
-
-	defer func() {
-		err = os.RemoveAll(tempDir)
-		assert.NoError(err, "failed to remove temp. dir.")
-	}()
-
-	emptyFilePath := filepath.Join(tempDir, "empty.json")
-
-	emptyFile, err := os.Create(emptyFilePath)
-	assert.NoError(err, "failed to create empty rule file")
-
-	_, err = emptyFile.Write([]byte("{}"))
-	assert.NoError(err, "failed to write empty rule file")
-
-	defer func() {
-		err = emptyFile.Close()
-		assert.NoError(err, "failed to close empty rule file")
-	}()
-
-	_, err = Init(WithReader(emptyFile))
-	assert.Error(err, "should have failed to decode JSON")
+	assert.Equal(want, have.Map())
 }
