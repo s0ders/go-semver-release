1d04f48dbef12eb9e4ec28ff8cb7e06074db9572
s0ders <39492740+s0ders@users.noreply.github.com>
2024-06-03 18:21:03 +0200

    fix: local cmd is now working on multi branch in parallel

--- cmd/local.go
+++ cmd/local.go
@@ -3,13 +3,16 @@ package cmd
 import (
 	"bytes"
 	"fmt"
+	"github.com/s0ders/go-semver-release/v2/internal/branch"
 	"github.com/spf13/viper"
 	"os"
+	"sync"
 
 	"github.com/ProtonMail/go-crypto/openpgp"
 	"github.com/go-git/go-git/v5"
 	"github.com/rs/zerolog"
 	"github.com/spf13/cobra"
+	"golang.org/x/sync/errgroup"
 
 	"github.com/s0ders/go-semver-release/v2/internal/ci"
 	"github.com/s0ders/go-semver-release/v2/internal/gpg"
@@ -19,29 +22,20 @@ import (
 )
 
 var (
-	rulesPath           string
-	tagPrefix           string
-	releaseBranch       string
-	armoredKeyPath      string
-	buildMetadata       string
-	prereleasIdentifier string
-	dryRun              bool
-	prereleaseMode      bool
+	tagPrefix      string
+	armoredKeyPath string
+	buildMetadata  string
+	dryRun         bool
 )
 
 func init() {
-	localCmd.Flags().StringVarP(&rulesPath, "rules", "r", "", "JSON release rules used for versioning")
 	localCmd.Flags().StringVarP(&tagPrefix, "tag-prefix", "t", "", "Prefix added to the version tag name")
-	localCmd.Flags().StringVarP(&releaseBranch, "release-branch", "b", "master", "Branch to fetch commits from")
 	localCmd.Flags().StringVar(&armoredKeyPath, "gpg-key-path", "", "Path to an armored GPG key used to sign produced tags")
 	localCmd.Flags().StringVar(&buildMetadata, "build-metadata", "", "Build metadata (e.g. build number) that will be appended to the SemVer")
-	localCmd.Flags().StringVar(&prereleasIdentifier, "prereleaseMode-identifier", "rc", "Identifier used for the prereleaseMode part of the SemVer (e.g. rc, alpha, beta)")
 	localCmd.Flags().BoolVarP(&dryRun, "dry-run", "d", false, "Only compute the next SemVer, do not push any tag")
-	localCmd.Flags().BoolVar(&prereleaseMode, "prereleaseMode", false, "Whether or not the SemVer is a prerelease")
 
 	cobra.CheckErr(viper.BindPFlag("tag-prefix", localCmd.Flags().Lookup("tag-prefix")))
 	cobra.CheckErr(viper.BindPFlag("rules", localCmd.Flags().Lookup("rules")))
-	cobra.CheckErr(viper.BindPFlag("prereleaseMode-identifier", localCmd.Flags().Lookup("prereleaseMode-identifier")))
 
 	rootCmd.AddCommand(localCmd)
 }
@@ -52,8 +46,11 @@ var localCmd = &cobra.Command{
 	Long:  "Tag a Git repository with the new semantic version number if a new release is found",
 	Args:  cobra.ExactArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) (err error) {
-		var rules rule.Rules
-		var entity *openpgp.Entity
+		var (
+			rules    rule.Rules
+			branches []branch.Branch
+			entity   *openpgp.Entity
+		)
 
 		logger := zerolog.New(cmd.OutOrStdout())
 
@@ -80,53 +77,99 @@ var localCmd = &cobra.Command{
 			return err
 		}
 
-		if viper.IsSet("rules") {
-			err = viper.UnmarshalKey("rules", &rules.Unmarshalled)
+		if !viper.IsSet("rules") {
+			rules = rule.Default
+		} else {
+			var rulesMarshalled map[string][]string
+
+			err = viper.UnmarshalKey("rules", &rulesMarshalled)
 			if err != nil {
 				return fmt.Errorf("unmarshalling rules: %w", err)
 			}
 
-			if err = rules.Validate(); err != nil {
-				return fmt.Errorf("validating rules: %w", err)
+			rules, err = rule.Unmarshall(rulesMarshalled)
+			if err != nil {
+				return err
 			}
-		} else {
-			rules = rule.Default
+		}
+
+		if !viper.IsSet("branches") {
+			return fmt.Errorf("missing branches key in configuration")
+		}
+
+		var branchesMarshalled []map[string]string
+
+		err = viper.UnmarshalKey("branches", &branchesMarshalled)
+		if err != nil {
+			return fmt.Errorf("unmarshalling branches: %w", err)
+		}
+
+		branches, err = branch.Unmarshall(branchesMarshalled)
+		if err != nil {
+			return fmt.Errorf("unmarshalling branches: %w", err)
 		}
 
 		tagger := tag.NewTagger(gitName, gitEmail, tag.WithTagPrefix(tagPrefix), tag.WithSignKey(entity))
 
-		parser := parser.New(logger, tagger, rules,
-			parser.WithReleaseBranch(releaseBranch),
-			parser.WithBuildMetadata(buildMetadata),
-			parser.WithPrereleaseMode(prereleaseMode),
-			parser.WithPrereleaseIdentifier(prereleasIdentifier))
-
-		semver, release, err := parser.ComputeNewSemver(repository)
+		err = viper.UnmarshalKey("branches", &branches)
 		if err != nil {
-			return fmt.Errorf("computing new semver: %w", err)
+			return fmt.Errorf("unmarshalling branches: %w", err)
 		}
 
-		err = ci.GenerateGitHubOutput(tagPrefix, semver, release)
-		if err != nil {
+		group, _ := errgroup.WithContext(cmd.Context())
+		var mu sync.RWMutex
+
+		// Launch a parser per branch to analyze
+		for _, branch := range branches {
+			group.Go(func() error {
+				parser := parser.New(logger, tagger, rules,
+					parser.WithReleaseBranch(branch.Pattern),
+					parser.WithPrereleaseMode(branch.Prerelease),
+					parser.WithPrereleaseIdentifier(branch.PrereleaseIdentifier),
+					parser.WithBuildMetadata(buildMetadata),
+				)
+
+				mu.RLock()
+				semver, release, err := parser.ComputeNewSemver(repository)
+				mu.RUnlock()
+				if err != nil {
+					return fmt.Errorf("computing new semver: %w", err)
+				}
+
+				// TODO: handle multi branch and parallelism
+				mu.Lock()
+				err = ci.GenerateGitHubOutput(branch.Pattern, tagPrefix, semver, release)
+				mu.Unlock()
+				if err != nil {
+					return err
+				}
+
+				switch {
+				case !release:
+					logger.Info().Str("current-version", semver.String()).Bool("new-release", false).Msg("no new release")
+					return nil
+				case release && dryRun:
+					logger.Info().Str("next-version", semver.String()).Bool("new-release", true).Msg("new release found, dry-run is enabled")
+					return nil
+				default:
+					logger.Info().Str("new-version", semver.String()).Bool("new-release", true).Msg("new release found")
+
+					mu.Lock()
+					err = tagger.TagRepository(repository, semver)
+					mu.Unlock()
+					if err != nil {
+						return fmt.Errorf("tagging repository: %w", err)
+					}
+
+					logger.Debug().Str("tag", tagPrefix+semver.String()).Msg("new tag added to repository")
+				}
+
+				return nil
+			})
+		}
+
+		if err := group.Wait(); err != nil {
 			return err
-		}
-
-		switch {
-		case !release:
-			logger.Info().Str("current-version", semver.String()).Bool("new-release", false).Msg("no new release")
-			return nil
-		case release && dryRun:
-			logger.Info().Str("next-version", semver.String()).Bool("new-release", true).Msg("new release found, dry-run is enabled")
-			return nil
-		default:
-			logger.Info().Str("new-version", semver.String()).Bool("new-release", true).Msg("new release found")
-
-			err = tagger.TagRepository(repository, semver)
-			if err != nil {
-				return fmt.Errorf("tagging repository: %w", err)
-			}
-
-			logger.Debug().Str("tag", tagPrefix+semver.String()).Msg("new tag added to repository")
 		}
 
 		return
