9767d0a62e85bda253b4b7544244dde5ca388519
s0ders <39492740+s0ders@users.noreply.github.com>
2024-06-23 18:52:08 +0200

    refactor: removed double error handling (cobra.CheckErr + if err...)

--- main.go
+++ main.go
@@ -1,16 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
 	"github.com/s0ders/go-semver-release/v3/cmd"
 )
 
 func main() {
-	err := cmd.Execute()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+	cmd.Execute()
 }
