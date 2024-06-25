5a13ef57ba4c3d82ac9610242a59e07ddffb84b5
s0ders <39492740+s0ders@users.noreply.github.com>
2024-06-25 17:47:45 +0200

    refactor: not displaying Git operations progress in stdout

--- internal/remote/remote.go
+++ internal/remote/remote.go
@@ -3,10 +3,12 @@ package remote
 
 import (
 	"fmt"
+	"io"
+	"os"
+
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/config"
 	"github.com/go-git/go-git/v5/plumbing/transport/http"
-	"os"
 )
 
 type Remote struct {
@@ -32,28 +34,30 @@ func (r *Remote) Clone(url string) (*git.Repository, error) {
 		return nil, fmt.Errorf("creating temporary directory: %w", err)
 	}
 
-	repository, err := git.PlainClone(tempDir, false, &git.CloneOptions{
+	r.repository, err = git.PlainClone(tempDir, false, &git.CloneOptions{
 		RemoteName: r.name,
 		Auth:       r.auth,
 		URL:        url,
-		Progress:   os.Stdout,
+		Progress:   io.Discard,
 	})
+	if err != nil {
+		return nil, fmt.Errorf("cloning repository: %w", err)
+	}
 
-	r.repository = repository
-
-	return repository, nil
+	return r.repository, nil
 }
 
 // PushTag pushes a given tag to the previously cloned repository's remote.
 func (r *Remote) PushTag(tagName string) error {
 	po := &git.PushOptions{
 		RemoteName: r.name,
-		Progress:   os.Stdout,
 		RefSpecs:   []config.RefSpec{config.RefSpec(fmt.Sprintf("refs/tags/%s:refs/tags/%s", tagName, tagName))},
 		Auth:       r.auth,
+		Progress:   io.Discard,
 	}
 
-	if err := r.repository.Push(po); err != nil {
+	err := r.repository.Push(po)
+	if err != nil {
 		return fmt.Errorf("pushing tag %q: %w", tagName, err)
 	}
 
