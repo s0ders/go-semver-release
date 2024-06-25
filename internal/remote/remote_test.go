6caf220c123de41ec17999106b84e6fda526572d
s0ders <39492740+s0ders@users.noreply.github.com>
2024-06-25 17:48:08 +0200

    test(remote): more test case without using an HTTP server, (local remote)

--- internal/remote/remote_test.go
+++ internal/remote/remote_test.go
@@ -1,7 +1,11 @@
 package remote
 
 import (
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/object"
+	"github.com/s0ders/go-semver-release/v4/internal/tag"
 	"testing"
+	"time"
 
 	assertion "github.com/stretchr/testify/assert"
 
@@ -11,39 +15,107 @@ import (
 func TestRemote_Clone_HappyScenario(t *testing.T) {
 	assert := assertion.New(t)
 
+	testRepository, err := gittest.NewRepository()
+	checkErr(t, err, "creating test repository")
+
+	defer func() {
+		err = testRepository.Remove()
+		checkErr(t, err, "removing test repository")
+	}()
+
 	remote := New("origin", "password")
 
-	testRepository, err := gittest.NewRepository()
-	if err != nil {
-		t.Fatalf("creating test repository: %s", err)
-	}
+	clonedRepository, err := remote.Clone(testRepository.Path)
+	checkErr(t, err, "cloning repository")
 
-	testRepository.StartRemoteWithAuth("username", "password")
-	defer testRepository.StopRemote()
-
-	clonedRepository, err := remote.Clone(testRepository.RemoteURL)
-	if err != nil {
-		t.Fatalf("failed to clone repository: %s", err)
-	}
-
-	assert.NoError(err)
 	assert.NotNil(clonedRepository)
+	assert.NoError(err)
 }
 
-func TestRemote_Clone_WrongToken(t *testing.T) {
+func TestRemote_Clone_NonExistingPath(t *testing.T) {
 	assert := assertion.New(t)
 
-	remote := New("origin", "wrong")
+	remote := New("origin", "password")
+	clonedRepository, err := remote.Clone("https://example.com")
+
+	assert.Nil(clonedRepository)
+	assert.Error(err)
+}
+
+func TestRemote_PushTag(t *testing.T) {
+	assert := assertion.New(t)
+
+	tagName := "v1.0.0"
 
 	testRepository, err := gittest.NewRepository()
-	if err != nil {
-		t.Fatalf("creating test repository: %s", err)
-	}
+	checkErr(t, err, "creating test repository")
 
-	testRepository.StartRemoteWithAuth("username", "password")
-	defer testRepository.StopRemote()
+	defer func() {
+		err = testRepository.Remove()
+		checkErr(t, err, "removing test repository")
+	}()
 
-	_, err = remote.Clone(testRepository.RemoteURL)
+	commitHash, err := testRepository.AddCommit("fix")
+	checkErr(t, err, "adding commit to test repository")
+
+	remote := New("origin", "password")
+
+	clonedRepository, err := remote.Clone(testRepository.Path)
+	checkErr(t, err, "cloning repository")
+
+	_, err = clonedRepository.CreateTag(tagName, commitHash, &git.CreateTagOptions{
+		Message: tagName,
+		Tagger: &object.Signature{
+			Name:  "Go Semver Release",
+			Email: "go-semver@release.ci",
+			When:  time.Now(),
+		},
+	})
+	checkErr(t, err, "creating tag on cloned repository")
+
+	err = remote.PushTag(tagName)
+	checkErr(t, err, "pushing tag to remote")
+
+	assert.True(tag.Exists(testRepository.Repository, tagName))
+}
+
+func TestRemote_PushTag_UnavailableRemote(t *testing.T) {
+	assert := assertion.New(t)
+
+	tagName := "v1.0.0"
+
+	testRepository, err := gittest.NewRepository()
+	checkErr(t, err, "creating test repository")
+
+	commitHash, err := testRepository.AddCommit("fix")
+	checkErr(t, err, "adding commit to test repository")
+
+	remote := New("origin", "password")
+
+	clonedRepository, err := remote.Clone(testRepository.Path)
+	checkErr(t, err, "cloning repository")
+
+	_, err = clonedRepository.CreateTag(tagName, commitHash, &git.CreateTagOptions{
+		Message: tagName,
+		Tagger: &object.Signature{
+			Name:  "Go Semver Release",
+			Email: "go-semver@release.ci",
+			When:  time.Now(),
+		},
+	})
+	checkErr(t, err, "creating tag on cloned repository")
+
+	// Removing remote
+	err = testRepository.Remove()
+	checkErr(t, err, "removing test repository")
+
+	err = remote.PushTag("v1.0.0")
 
 	assert.Error(err)
+}
+
+func checkErr(t *testing.T, err error, msg string) {
+	if err != nil {
+		t.Fatalf("%s: %s", msg, err)
+	}
 }
