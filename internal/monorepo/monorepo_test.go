b1773c7eb5fe6efcf76afbc0f1845c4168158ca8
s0ders <39492740+s0ders@users.noreply.github.com>
2024-09-18 22:14:22 +0200

    test: unit tests for the "monorepo" package in charge of parsing monorepo configuration

---
+++ internal/monorepo/monorepo_test.go
@@ -0,0 +1,48 @@
+package monorepo
+
+import (
+	"path/filepath"
+	"testing"
+
+	assertion "github.com/stretchr/testify/assert"
+)
+
+func TestMonorepo_Unmarshall(t *testing.T) {
+	assert := assertion.New(t)
+
+	have := []map[string]string{{"name": "bar", "path": "./bar/"}, {"name": "foo", "path": "./xyz/foo/"}}
+
+	localizedPath, _ := filepath.Localize("xyz/foo")
+
+	want := []Project{
+		{Name: "bar", Path: "bar"},
+		{Name: "foo", Path: localizedPath},
+	}
+
+	branches, err := Unmarshall(have)
+	if err != nil {
+		t.Fatalf("unmarshalling projects: %s", err)
+	}
+
+	assert.Equal(want, branches)
+}
+
+func TestBranch_UnmarshallErrors(t *testing.T) {
+	assert := assertion.New(t)
+
+	type test struct {
+		have []map[string]string
+		want error
+	}
+
+	tests := []test{
+		{have: []map[string]string{{"path": "./foo/"}}, want: ErrNoName},
+		{have: []map[string]string{{"name": "foo"}}, want: ErrNoPath},
+		{have: []map[string]string{{"name": "foo", "path": "./foo/"}}, want: nil},
+	}
+
+	for _, tc := range tests {
+		_, err := Unmarshall(tc.have)
+		assert.Equal(tc.want, err)
+	}
+}
