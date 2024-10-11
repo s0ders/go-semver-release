6c77ae441eacd66de1c65f441d02c96eb93547a1
s0ders <39492740+s0ders@users.noreply.github.com>
2024-10-11 18:14:17 +0200

    ci: displaying latest stable release version in download script

--- build/download_latest_release.sh
+++ build/download_latest_release.sh
@@ -5,7 +5,9 @@ json_url="https://api.github.com/repos/s0ders/go-semver-release/releases/latest"
 asset_name="go-semver-release-linux-amd64"
 
 # Get the download URL
-download_url=$(curl -s "$json_url" | jq -r ".assets[] | select(.name == \"$asset_name\") | .browser_download_url")
+api_response=$(curl -s --no-progress-meter "$json_url")
+download_url=$(echo "$api_response" | jq -r ".assets[] | select(.name == \"$asset_name\") | .browser_download_url")
+asset_version=$(echo "$api_response" | jq -r ".tag_name")
 
 # Check if download URL was found
 if [ -z "$download_url" ] || [ "$download_url" = "null" ]; then
@@ -13,13 +15,11 @@ if [ -z "$download_url" ] || [ "$download_url" = "null" ]; then
     exit 1
 fi
 
-echo "Downloading from: $download_url"
-
 # Download the file
 curl -L -o "$asset_name" "$download_url"
 
 if [ $? -eq 0 ]; then
-    echo "Successfully downloaded: $asset_name"
+    echo "Successfully downloaded: $asset_name $asset_version"
 else
     echo "Error downloading file"
     exit 1
