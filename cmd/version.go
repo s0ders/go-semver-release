1db0e1937207c774f92abe6b12cb9a7067dee9bb
s0ders <39492740+s0ders@users.noreply.github.com>
2024-10-14 21:56:19 +0200

    refactor: rephrased "version" command long description

--- cmd/version.go
+++ cmd/version.go
@@ -16,7 +16,7 @@ func NewVersionCmd() *cobra.Command {
 	versionCmd := &cobra.Command{
 		Use:   "version",
 		Short: "Display CLI current version",
-		Long:  "Display CLI current version and the associated build number and commit hash",
+		Long:  "Display CLI current version, the associated build number and commit hash",
 		Args:  cobra.ExactArgs(0),
 		RunE: func(cmd *cobra.Command, args []string) (err error) {
 			fmt.Fprintf(cmd.OutOrStdout(), "Version: %s\nBuild: %s\nCommit: %s\n", cmdVersion, buildNumber, buildCommitHash)
