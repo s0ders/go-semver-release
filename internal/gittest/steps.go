70986f6a31f2535b4c37895d904d9a1e8e6d936f
Dennis Markgraf <info@dennismarkgraf.com>
2025-06-18 16:11:19 +0000

    feat(gittest): test steps implementation

---
+++ internal/gittest/steps.go
@@ -0,0 +1,259 @@
+package gittest
+
+import (
+	"fmt"
+	"os/exec"
+)
+
+type Step interface {
+	// Execution method that will be called by ExecuteSteps
+	exec(r *TestRepository) error
+}
+
+type branchStep struct {
+	// Which branch to checkout before running the command
+	// If branch is "" the command will be executed on the current branch
+	branch string
+}
+
+type CheckoutStep struct {
+	branchStep
+
+	// Name of the (new) branch
+	name string
+
+	// Whenever just checking out branch or create a new one with given name from HEAD
+	create bool
+}
+
+type CommitStep struct {
+	branchStep
+
+	// Which commit type to prepend. E.g. `feat`, `fix`...
+	commitType string
+}
+
+type CommitWithFileStep struct {
+	CommitStep
+
+	// File to be created for this commit.
+	filePath string
+}
+
+type MergeStep struct {
+	branchStep
+
+	// Source branch to merge into current HEAD.
+	source string
+
+	// Whenever only to allow a fast-forwad merge.
+	fastForward bool
+}
+
+type TagStep struct {
+	branchStep
+
+	// Name to be used for the new tag.
+	name string
+}
+
+type CallbackStep[E any] struct {
+	branchStep
+
+	// Optional expected to be used in callbacks assert.
+	expected E
+
+	// Callback function that will be called with the expected parameter.
+	cb func(E) error
+}
+
+// NewCommitStep creates a new commit step.
+// This step creates a new commit to the current or provided branch
+// with the given commit type (e.g. `feat`, `fix`...).
+func NewCommitStep(branch string, commitType string) *CommitStep {
+	return &CommitStep{
+		branchStep: branchStep{
+			branch: branch,
+		},
+		commitType: commitType,
+	}
+}
+
+// NewCheckoutStep creates a new checkout step
+// This step checkouts a branch or optionally forces creation of a new branch on the current HEAD
+func NewCheckoutStep(branch string, name string, create bool) *CheckoutStep {
+	return &CheckoutStep{
+		branchStep: branchStep{
+			branch: branch,
+		},
+		name:   name,
+		create: create,
+	}
+}
+
+// NewCommitWithFileStep creates a new commit step.
+// This step creates a new commit to the current or provided branch
+// with the given commit type (e.g. `feat`, `fix`...) and creates a dummy
+// file on the provided file path.
+func NewCommitWithFileStep(branch string, commitType string, filePath string) *CommitWithFileStep {
+	return &CommitWithFileStep{
+		CommitStep: CommitStep{
+			branchStep: branchStep{
+				branch: branch,
+			},
+			commitType: commitType,
+		},
+		filePath: filePath,
+	}
+}
+
+// NewMergeStep creates a new merge step.
+// This step merges a source branch into another branch or to the current HEAD if no
+// branch is provided.
+func NewMergeStep(branch string, source string, fastForward bool) *MergeStep {
+	return &MergeStep{
+		branchStep: branchStep{
+			branch: branch,
+		},
+		source:      source,
+		fastForward: fastForward,
+	}
+}
+
+// NewTagStep creates a new merge step.
+// This step greates a new tag with the given name on the given branch.
+func NewTagStep(branch string, name string) *TagStep {
+	return &TagStep{
+		branchStep: branchStep{
+			branch: branch,
+		},
+		name: name,
+	}
+}
+
+// NewCallbackStep creates a new merge step.
+// This step holds a callback that can be called to assert the current state.
+func NewCallbackStep[E any](branch string, expected E) *CallbackStep[E] {
+	return &CallbackStep[E]{
+		branchStep: branchStep{
+			branch: branch,
+		},
+		expected: expected,
+	}
+}
+
+// BranchStep.exec executes branch step.
+func (s *branchStep) exec(r *TestRepository) error {
+	if s.branch != "" {
+		err := r.CheckoutOrCreateBranch(s.branch)
+		if err != nil {
+			return fmt.Errorf("checking out %s branch: %w", s.branch, err)
+		}
+	}
+	return nil
+}
+
+// CommitStep.exec executes commit step.
+func (s *CommitStep) exec(r *TestRepository) error {
+	err := s.branchStep.exec(r)
+	if err != nil {
+		return nil
+	}
+
+	_, err = r.AddCommit(s.commitType)
+	if err != nil {
+		return fmt.Errorf("creating commit: %w", err)
+	}
+	return nil
+}
+
+// CheckoutStep.exec executes checkout step.
+func (s *CheckoutStep) exec(r *TestRepository) error {
+	err := s.branchStep.exec(r)
+	if err != nil {
+		return nil
+	}
+
+	err = r.CheckoutBranch(s.name, s.create)
+	if err != nil {
+		return fmt.Errorf("checkout branch: %w", err)
+	}
+	return nil
+}
+
+// CommitWithFileStep.exec executes commit with file step.
+func (s *CommitWithFileStep) exec(r *TestRepository) error {
+	err := s.branchStep.exec(r)
+	if err != nil {
+		return nil
+	}
+
+	_, err = r.AddCommitWithSpecificFile(s.commitType, s.filePath)
+	if err != nil {
+		return fmt.Errorf("creating commit with file: %w", err)
+	}
+	return nil
+}
+
+// MergeStep.exec executes merge step.
+func (s *MergeStep) exec(r *TestRepository) error {
+	err := s.branchStep.exec(r)
+	if err != nil {
+		return nil
+	}
+
+	args := []string{"merge"}
+	if s.fastForward {
+		args = append(args, "--ff-only")
+	} else {
+		args = append(args, "-X", "theirs")
+	}
+	args = append(args, s.source)
+
+	cmd := exec.Command("git", args...)
+	cmd.Dir = r.Path
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("%s : %w", string(output), err)
+	}
+	return nil
+}
+
+// TagStep.exec executes tag step.
+func (s *TagStep) exec(r *TestRepository) error {
+	err := r.AddTagToBranch(s.name, s.branch)
+	if err != nil {
+		return fmt.Errorf("adding tag: %w", err)
+	}
+	return nil
+}
+
+// CallbackStep.exec executes tag step.
+func (s *CallbackStep[E]) exec(r *TestRepository) error {
+	err := s.branchStep.exec(r)
+	if err != nil {
+		return nil
+	}
+
+	if s.cb != nil {
+		err := s.cb(s.expected)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// ExecuteSteps executes steps on test repository.
+func ExecuteSteps[E any](r *TestRepository, steps []Step, cb func(E) error) error {
+	for _, step := range steps {
+		if callbackStep, ok := step.(*CallbackStep[E]); ok {
+			callbackStep.cb = cb
+		}
+		err := step.exec(r)
+		if err != nil {
+			return fmt.Errorf("executing step: %w", err)
+		}
+	}
+	return nil
+}
